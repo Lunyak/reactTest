887b38bb616df5a018de858853dac038c0c995a4
Lunyak <wampwar11@gmail.com>
2020-11-09 20:28:22 +0300

    Updates

---
+++ precache-manifest.37aa0051c66cf03354299e4d427ad69e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccc2f464543f70a734d79c7400dc03a2",
+    "url": "/reactTest/index.html"
+  },
+  {
+    "revision": "fad0e0cbef513d91676f",
+    "url": "/reactTest/static/css/main.9aa4c3c9.chunk.css"
+  },
+  {
+    "revision": "32a67d18ed7fade8131d",
+    "url": "/reactTest/static/js/2.3efcad13.chunk.js"
+  },
+  {
+    "revision": "8c741daff44cdfbc8ec6cebfb62400f7",
+    "url": "/reactTest/static/js/2.3efcad13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fad0e0cbef513d91676f",
+    "url": "/reactTest/static/js/main.6c680498.chunk.js"
+  },
+  {
+    "revision": "c8d4acb3ee723c8d5437",
+    "url": "/reactTest/static/js/runtime-main.96a80951.js"
+  },
+  {
+    "revision": "2ba119d915d4bb23a13cf075ee63cc7f",
+    "url": "/reactTest/static/media/SFNSDisplay-Heavy.2ba119d9.woff2"
+  },
+  {
+    "revision": "ecba511686172bb234a27875b5830aee",
+    "url": "/reactTest/static/media/SFNSDisplay-Heavy.ecba5116.ttf"
+  },
+  {
+    "revision": "f17f49c6c6dcc756a9e722e02ba5fbee",
+    "url": "/reactTest/static/media/SFNSDisplay-Heavy.f17f49c6.woff"
+  },
+  {
+    "revision": "6f7af5463d904905ea495a73e61c1baf",
+    "url": "/reactTest/static/media/SFNSDisplay-Medium.6f7af546.woff2"
+  },
+  {
+    "revision": "9a7c7a8f26ccf885736c3daa86490239",
+    "url": "/reactTest/static/media/SFNSDisplay-Medium.9a7c7a8f.woff"
+  },
+  {
+    "revision": "d7064abbe763af6f22ddfbe0b30ac3cc",
+    "url": "/reactTest/static/media/SFNSDisplay-Medium.d7064abb.ttf"
+  },
+  {
+    "revision": "2e3c39fcf1678ad31e51d78a4ee22d99",
+    "url": "/reactTest/static/media/SFNSDisplay-Semibold.2e3c39fc.woff2"
+  },
+  {
+    "revision": "ab5a1a4f2aaf90ca23ad238a31004133",
+    "url": "/reactTest/static/media/SFNSDisplay-Semibold.ab5a1a4f.woff"
+  },
+  {
+    "revision": "ad2755e716f47c0681f08a51f9c728da",
+    "url": "/reactTest/static/media/SFNSDisplay-Semibold.ad2755e7.ttf"
+  },
+  {
+    "revision": "bcaa2da4c2e9364bcc81cc46073c59b0",
+    "url": "/reactTest/static/media/SFNSText-Light.bcaa2da4.woff2"
+  },
+  {
+    "revision": "ea1542e6eaba2086f3023b7d7543c669",
+    "url": "/reactTest/static/media/SFNSText-Light.ea1542e6.woff"
+  },
+  {
+    "revision": "f7d862e87c5eebc5e1720a3aa464263f",
+    "url": "/reactTest/static/media/SFNSText-Light.f7d862e8.ttf"
+  }
+]);
